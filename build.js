e8cc8aab37d599e9592e31b6a5a00311ede3cadc
bonbonez <bonbonez@gmail.com>
2018-02-02 16:05:55 +0300

    travis test

--- build.js
+++ build.js
@@ -1,31 +1,3 @@
-const {
-  exec,
-  build,
-  cd,
-  lifecycle,
-  isPushEventBuild,
-  gitPush,
-  npmPublish,
-  resolveNpmTag
-} = require('@grabrinc/grabr-build-tools');
+const {build, lifecycle} = require('@grabrinc/grabr-build-tools');
 
-build({
-  ...lifecycle,
-  publish() {
-    if (isPushEventBuild()) {
-      gitPush();
-      const dir = './target/out';
-      const curDir = process.cwd();
-      if (dir) {
-        exec('echo "copying "');
-        exec(`cp README.md LICENSE package.json ${dir}`);
-        cd(dir);
-        exec('echo "changed directory"');
-      }
-      exec("echo '=========== current dir'")
-      exec('ls -la');
-      exec(`npm publish --tag=${resolveNpmTag()}`);
-      cd(curDir);
-    }
-  }
-});
+build(lifecycle);
