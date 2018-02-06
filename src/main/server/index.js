c54fd74ad65e17da562b2602d32e2ac139732ebc
bonbonez <bonbonez@gmail.com>
2018-02-06 10:39:36 +0300

    fix

--- src/main/server/index.js
+++ src/main/server/index.js
@@ -1,4 +1,3 @@
-export {Logger, Logger as default} from '../Logger';
 import {PROCESSOR_FACTORIES as DEFAULT_PROCESSOR_FACTORIES} from '../index';
 import {createSentryNodeLogger} from '../processors/createSentryNodeLogger';
 
